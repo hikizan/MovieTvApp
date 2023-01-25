b4c4639a7c3b7fc16333a911361d083e92f40000
Akhmad Fauzan Prayogi <akhmadfauzanprayogi@gmail.com>
2023-01-25 16:25:10 +0700

    init project

---
+++ app/src/main/java/com/hikizan/movietvapp/presentation/main/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.hikizan.movietvapp.presentation.main
+
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import com.hikizan.movietvapp.R
+import com.hikizan.movietvapp.base.HikizanActivity
+import com.hikizan.movietvapp.databinding.ActivityMainBinding
+import com.hikizan.movietvapp.presentation.main.adapter.SectionPagerAdapter
+
+class MainActivity : HikizanActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+
+    private val viewPagerAdapter: SectionPagerAdapter by lazy {
+        SectionPagerAdapter(
+            supportFragmentManager,
+            lifecycle
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+
+    }
+
+    override fun initIntent() {
+    }
+
+    override fun initUI() {
+        binding?.apply {
+            val tabTitles = listOf(
+                getString(R.string.title_movie),
+                getString(R.string.title_tv)
+            )
+
+            vpMain.apply {
+                adapter = viewPagerAdapter
+                isUserInputEnabled = false
+            }
+
+            TabLayoutMediator(tabMain, vpMain) { tab, position ->
+                tab.text =tabTitles[position]
+            }.attach()
+        }
+    }
+
+    override fun initProcess() {
+    }
+
+    override fun initObservers() {
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/hikizan/movietvapp/presentation/main/adapter/SectionPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.hikizan.movietvapp.presentation.main.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.hikizan.movietvapp.presentation.movie.MovieFragment
+import com.hikizan.movietvapp.presentation.tv.TvshowFragment
+
+class SectionPagerAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return if (position == 0) {
+            MovieFragment()
+        } else {
+            TvshowFragment()
+        }
+    }
+}
