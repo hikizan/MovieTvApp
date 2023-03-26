e425a41c3f17e77f13fdcd136e385ce68031458b
Akhmad Fauzan Prayogi <akhmadfauzanprayogi@gmail.com>
2023-03-26 23:24:09 +0700

    lab(performance-tuning): doing inspect code and adjust some suggest.

--- core/src/main/java/com/hikizan/movietvapp/core/data/movietv/remote/RemoteDataSource.kt
+++ core/src/main/java/com/hikizan/movietvapp/core/data/movietv/remote/RemoteDataSource.kt
@@ -41,7 +41,7 @@ class RemoteDataSource(private val movieTvApiClient: MovieTvApiClient) {
                 val tvShowsResponse =
                     movieTvApiClient.getTvShows(BuildConfig.API_KEY)
 
-                if (tvShowsResponse.results != null) {
+                if (tvShowsResponse.results.isNotEmpty()) {
                     emit(ApiResponseResult.Success(tvShowsResponse.results))
                 } else {
                     emit(ApiResponseResult.Empty)
@@ -49,34 +49,6 @@ class RemoteDataSource(private val movieTvApiClient: MovieTvApiClient) {
             } catch (e: Exception) {
                 emit(ApiResponseResult.Error(e.toString()))
                 Log.e(TAG, "getTvShows: $e")
-            }
-        }.flowOn(Dispatchers.IO)
-    }
-
-    suspend fun getDetailMovie(movieId: Int): Flow<ApiResponseResult<MovieItemResponse>> {
-        return flow {
-            try {
-                val detailMovieResponse =
-                    movieTvApiClient.getMovie(movieId, BuildConfig.API_KEY)
-
-                emit(ApiResponseResult.Success(detailMovieResponse))
-            } catch (e: Exception) {
-                emit(ApiResponseResult.Error(e.toString()))
-                Log.e(TAG, "getDetailMovie: $e")
-            }
-        }.flowOn(Dispatchers.IO)
-    }
-
-    suspend fun getDetailTvShow(tvId: Int): Flow<ApiResponseResult<TvShowItemResponse>> {
-        return flow {
-            try {
-                val detailTvShowResponse =
-                    movieTvApiClient.getTvShow(tvId, BuildConfig.API_KEY)
-
-                emit(ApiResponseResult.Success(detailTvShowResponse))
-            } catch (e: Exception) {
-                emit(ApiResponseResult.Error(e.toString()))
-                Log.e(TAG, "getDetailTvShow: $e")
             }
         }.flowOn(Dispatchers.IO)
     }
